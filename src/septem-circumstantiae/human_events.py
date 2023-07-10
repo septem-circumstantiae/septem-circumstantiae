0d5da4131ff035ae31788d2c9f06dc92ba05e9b2
Alex Fedotov <alxfed@gmail.com>
2023-07-10 14:17:03 -0500

    Events

---
+++ src/septem-circumstantiae/human_events.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Python
+
+"""Copyright (c) Alexander Fedotov.
+This source code is licensed under the license found in the
+LICENSE file in the root directory of this source tree.
+"""
+
+# https://alxfed.github.io/blog/posts/2019/02/22/Septem-Circumstantiae.html
+
+
+class HumanEvent:
+    """"""
+    who: str    = ''  # quis
+    what: str   = ''  # quid
+    when: str   = ''  # quando
+    where: str  = ''  # ubi
+    why: str    = ''  # cur (for the sake of what?)
+    how: str    = ''  # quem ad modum (in what way?)
+    by_what_means: str = ''  # quibus adminiculis
+
+    def __init__(self, *args, **kwargs):
+        pass
---
+++ src/septem-circumstantiae/natural_events.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Python
+
+"""Copyright (c) Alexander Fedotov.
+This source code is licensed under the license found in the
+LICENSE file in the root directory of this source tree.
+"""
+
+# natural events have some of the properties ommitted.
+
+
+class NaturalEvent:
+    """"""
+    # who: str    = ''  # quis
+    what: str   = ''  # quid
+    when: str   = ''  # quando
+    where: str  = ''  # ubi
+    why: str    = ''  # cur
+    how: str    = ''  # quem ad modum (in what way?)
+    # by_what_means: str = ''  # quibus adminiculis
+
+    def __init__(self, *args, **kwargs):
+        pass
