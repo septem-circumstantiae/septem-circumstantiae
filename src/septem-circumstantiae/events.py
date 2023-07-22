c0601354d8a4e9962f2f5bde0d9db93389a1059a
Alex Fedotov <alxfed@gmail.com>
2023-07-22 15:39:08 -0500

    kwargs

--- src/septem-circumstantiae/events.py
+++ src/septem-circumstantiae/events.py
@@ -7,58 +7,52 @@ LICENSE file in the root directory of this source tree.
 """
 
 # https://alxfed.github.io/blog/posts/2019/02/22/Septem-Circumstantiae.html
+circumstantiae = [
+    'who', 'what', 'when', 'where', 'why', 'how', 'by_what_means'
+]
 
 
 class Event:
     """"""
-    who: str    = ''  # quis. Who did it or who is reporting a natural event?
-    what: str   = ''  # quid. What happened and was observed?
-    when: str   = ''  # quando. When did it happen or started happening? Does it still continue or can happen again?
-    where: str  = ''  # ubi. Where did it take place?
-    why: str    = ''  # cur (for the sake of what?). Is there a cause or reason for the event?
-    how: str    = ''  # quem ad modum (in what way?). How did it happen?
-    by_what_means: str = ''  # quibus adminiculis. How was it achieved? Were there any other factors involved?
+    who: str            = ''  # quis. Who did it or who is reporting a natural event?
+    what: str           = ''  # quid. What happened and was observed?
+    when: str           = ''  # quando. When did it happen or started happening? Does it still continue or can happen again?
+    where: str          = ''  # ubi. Where did it take place?
+    why: str            = ''  # cur (for the sake of what?). Is there a cause or reason for the event?
+    how: str            = ''  # quem ad modum (in what way?). How did it happen?
+    by_what_means: str  = ''  # quibus adminiculis. How was it achieved? Were there any other factors involved?
 
-    def __init__(self, *args, **kwargs):
-        pass
+    def __init__(self, **kwargs):
+        for key,value in kwargs.items():
+            setattr(self, key, value)
 
-    def answer_who(self, *args, **kwargs):
-        pass
-
-    def answer_what(self, *args, **kwargs):
-        pass
-
-    def answer_when(self, *args, **kwargs):
-        pass
-
-    def answer_where(self, *args, **kwargs):
-        pass
-
-    def answer_why(self, *args, **kwargs):
-        pass
-
-    def answer_how(self, *args, **kwargs):
-        pass
-
-    def answer_by_what_means(self, *args, **kwargs):
-        pass
-
-    def __call__(self, *args, **kwargs):
-        self.answer_who(self, *args, **kwargs)
-        self.answer_what(self, *args, **kwargs)
-        self.answer_how(self, *args, **kwargs)
-        self.answer_when(self, *args, **kwargs)
-        self.answer_where(self, *args, **kwargs)
-        self.answer_why(self, *args, **kwargs)
-        self.answer_how(self, *args, **kwargs)
-        self.answer_by_what_means(self, *args, **kwargs)
+    def __call__(self, **kwargs):
+        for key,value in kwargs.items():
+            setattr(self, key, value)
         return self
 
     def __repr__(self, *args, **kwargs):
-        return f"""Event(who={self.who},/n 
-            what={self.what},/n /n
-            when={self.when},/n /n
-            where={self.where},/n /n
-            why={self.why},/n /n
-            how={self.how},/n /n
-            by_what_means={self.by_what_means})/n /n"""
+        return f"""     Event:
+            who - {self.who},
+            what - {self.what},
+            when - {self.when},
+            where - {self.where},
+            why - {self.why},
+            how - {self.how},
+            by what means - {self.by_what_means}
+            """
+
+
+if __name__ == '__main__':
+    keyword_arguments = {
+        'who': 'I',
+        'what': 'do',
+        'when': 'now',
+        'where': 'here',
+        'why': 'because',
+        'how': 'in a most simple way',
+        'by_what_means': 'manually'
+    }
+    object = Event(**keyword_arguments)
+    print(object)
+    pass
